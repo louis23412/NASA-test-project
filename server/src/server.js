bc1238c91d8f710ec18e7f448a2aea6334e1a172
Louis <louis23412@gmail.com>
2025-04-02 14:26:40 +0200

    added connection to mongoDB with mongoose module

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -6,9 +7,20 @@ const { loadPlanetsData } = require('./models/planets.model')
 
 const PORT = process.env.PORT || 8000; // Checks if there was an environment variable for PORT, else just use port 8000
 
+const MONGO_URL = 'connection string here'
+
 const server = http.createServer(app); // We pass the express app to the server as a listener / handler
 
+mongoose.connection.once('open', () => { // Event emitter (.once will only be triggered once when the code is executed)
+    console.log('Connected to database!')
+})
+
+mongoose.connection.on('error', (err) => { // We can add these event handlers anywhere in the file, as long as we required / imported the mongoose module
+    console.error(err)
+})
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL); // Connect to MongoDB
     await loadPlanetsData(); // Wait for planets data to populate before the server starts listening to requests
 
     server.listen(PORT, () => {
